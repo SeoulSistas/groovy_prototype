f4fab7919e8e05074c1bf31b4e871604d611c2e8
Rupa Shankar <rupsshankar@gmail.com>
2014-03-09 13:14:36 -0700

    Added a little map to the index page. Based on the center of the map, displays the questions (based on Dennis' backend code) for which the center is within the radius. Automatically updates when the map center changes.
    Needs to be updated once Dennis updates his code
    to get all the questions for a bounding box, not
    just the center.
    
    Right now, just displays all possible questions
    at the bottom of the page.
    
    ---
    
    Removed filter and radius from head and added them
    to application.js -> got rid of filter and radius
    include errors. No more jquery-rails tag.
    
    ---
    
    Fixed slider color

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,39 @@
+var map = null;
+var geocoder = null; 
+
+function filterCodeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+function initBoundingBox() {
+    geocoder = new google.maps.Geocoder();
+    var mapDiv = document.getElementById('filter-map-canvas');
+    if (mapDiv != null) {
+        map = new google.maps.Map(mapDiv, {
+            center: new google.maps.LatLng(37.424105999999, -122.1660756),
+            zoom: 10, 
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        google.maps.event.addDomListener(map, 'bounds_changed', whenBoundsChange);
+    }
+
+}
+
+function whenBoundsChange() {
+    var center = map.getCenter();
+    var url = "filter_questions?lat=" + encodeURIComponent(center.lat()) + "&lng=" + encodeURIComponent(center.lng());
+    jQuery.get(url, function(data) {
+        document.getElementById('all_questions').innerHTML = data; 
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initBoundingBox);
+
