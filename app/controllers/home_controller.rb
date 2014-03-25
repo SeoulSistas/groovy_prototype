6e3e5cffa48da5ab98146c86b595b7db64576b4b
Rupa Shankar <rupsshankar@gmail.com>
2014-03-25 14:09:27 +0800

    Retrieves live questions only and updates the questions for a particular location upon each retrieval. With this, TTL is fully implemented and working.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,38 @@ class HomeController < ApplicationController
   end
 
   def search
-    @questions = Question.all
-    # @questions = Array.new
-    # all_questions = Question.all
-    # if params[:n]!=nil and params[:s]!=nil and params[:e]!=nil and params[:w]!=nil
-    #   for question in all_questions
-    #     if question.latitude.between?(params[:s].to_f,params[:n].to_f) and question.longitude.between?(params[:w].to_f, params[:e].to_f)
-    #       @questions.push(question)
-    #     end
-    #   end
-    # end
+   questions_at_location = Array.new
+    all_questions = Question.where(is_alive: true)
+    if params[:n]!=nil and params[:s]!=nil and params[:e]!=nil and params[:w]!=nil
+      for question in all_questions
+        puts question.created_at
+        if question.latitude.between?(params[:s].to_f,params[:n].to_f) and question.longitude.between?(params[:w].to_f, params[:e].to_f)
+          questions_at_location.push(question)
+        end
+      end
+    end
+    @questions = get_and_update_live_questions(questions_at_location)
     @questions.sort! {|a,b| b.created_at <=> a.created_at}
     render :partial => 'searched_questions', :locals => {:questions => @questions}
+  end
+
+  
+  def get_and_update_live_questions(questions_at_location)
+    live_questions = Array.new
+    curr_time = DateTime.now
+    for question in questions_at_location
+      if !question.ttl.nil?
+        end_time = question.created_at + question.ttl * 60
+        if curr_time.between?(question.created_at, end_time)
+          question.is_alive = true
+          live_questions.push(question)
+        else
+          question.is_alive = false
+        end
+        question.save
+      end
+    end
+    return live_questions
   end
 
 end
