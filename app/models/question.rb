d50b79429b136e95666a8d637b28599fd24e4aa6
Changwhan Yea <yeaz90@gmail.com>
2014-03-27 19:07:42 +0900

    working on question styling

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,5 +8,20 @@ class Question < ActiveRecord::Base
 		   :distance_field_name => :distance,
 		   :lat_column_name => :latitude,
  		   :lng_column_name => :longitude 
+           
+  def display_ttl
+    end_time = self.created_at + self.ttl * 60
+    curr_time = Time.now
+    mins = ((end_time - curr_time) / 60).to_i
+    if mins < 60
+      mins.to_s + ' mins left'
+    elsif mins < 1440
+      hours = mins / 60
+      hours.to_s + ' hours left'
+    else
+      days = mins/ 3600
+      days.to_s + ' days left'
+    end 
+  end
 
 end
